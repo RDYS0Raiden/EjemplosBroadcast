61b3259aa462401c8c7d16ddc972c0615b5b3ecd
Ian Martinez <mli2024827@est.univalle.edu>
2022-10-01 11:03:46 -0400

    .

---
+++ app/src/main/java/com/example/ejemplosbroadcast/MyBroadcast.kt
@@ -0,0 +1,27 @@
+package com.example.ejemplosbroadcast
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.os.BatteryManager
+
+//para esta clase se comporte como un
+//Broadcast Reciver hay que extender
+//una clase abstracta que ustedes ya la
+// han usado 3 veces en el ejemplo Broadcast
+
+class MyBroadcast : BroadcastReceiver(){
+    override fun onReceive(context: Context?, intent: Intent?) {
+        // TODO ("Configurar nivel de bateria")
+        when(intent?.action){
+            Intent.ACTION_BATTERY_CHANGED-> showBatteryLevel(intent)
+            
+        }
+    }
+
+    private fun showBatteryLevel(intent: Intent) {
+
+    }
+
+
+}
