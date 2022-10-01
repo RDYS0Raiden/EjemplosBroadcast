d8fc591dc6cf3993e18b9c7728486c39435f827f
Ian Martinez <mli2024827@est.univalle.edu>
2022-10-01 10:55:16 -0400

    .

--- app/src/main/java/com/example/ejemplosbroadcast/MainActivity.kt
+++ app/src/main/java/com/example/ejemplosbroadcast/MainActivity.kt
@@ -4,11 +4,19 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.net.wifi.WifiManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.speech.tts.TextToSpeech
+import android.widget.Toast
 import com.example.ejemplosbroadcast.databinding.ActivityMainBinding
+import java.util.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
+
+
+    private lateinit var tts: TextToSpeech
+
 private lateinit var binding: ActivityMainBinding
 
 //definir un objeto de tipo BroadcastReciver
@@ -22,31 +30,86 @@ private val getAirplaneMode=object :BroadcastReceiver() {
         airplaneMode?.let{
             val message = if (it) "Modo Avion Activado" else "Modo Avion Desactivado"
             binding.txtModoAvion.text=message
+
+
         }
+
     }
 }
+    // Configurar un BroadcastReceiver para el evento de cambio de tiempo a este se le denomina
+    // Time Tick
+    private val getTimeChange = object: BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            var a="El tiempo a cambiado"
+            binding.txtTimeTick.text="El tiempo ha cambiado"
+
+            tts.speak(a, TextToSpeech.QUEUE_FLUSH,null,"")
+
+            }
+    }
+    //vamos a configurar el BroadcastReciver
+    //para saber cuando el wifi esta habilitado o no
+    private val getWifiMode= object: BroadcastReceiver(){
+        override fun onReceive(p0: Context?, p1: Intent?) {
+            //Wifi trabaja pasando a traves de un registro en el
+            //Intent un dato de tipo numerico ese dato
+            // te va a indicar aspectos  como si  esta habilitado,
+            // si no esta habilitado, configuraciones de la red,
+            // si el servicio esta dañado o no se reconoce
+            // Servicio Wifi: se gestiona a traves de un controlador
+            // que es una clase que lo administra -> WifiManager
+            val wifiMode= intent?.getIntExtra(WifiManager.EXTRA_WIFI_STATE,WifiManager.WIFI_STATE_UNKNOWN)
+            wifiMode?.let {
+                val message= when(it){
+                    WifiManager.WIFI_STATE_ENABLED->"Wifi habilitado"
+                    WifiManager.WIFI_STATE_DISABLED->"Wifi esta desabilitado"
+                    WifiManager.WIFI_STATE_UNKNOWN->"Servicio Wifi error"
+                    else->"No dispone de Wifi, actualizate"
+
+                }
+                binding.txtWifi.text= message
+
+            }
+        }
+    }
+
+    override fun onInit(status: Int) {
+        var resultado = if (status == TextToSpeech.SUCCESS) {
+            //se puede escribir 500 lineas de codigo pero la ultima tiene que ser el
+            //valor que quieres que asuma esa varianle
+            //por defecto el lenguaje es ingles...
+            //si quieres modificar el lenguaje se hace esto: tts.language= Locale.US
+            //tts.language= Locale.US
+            tts.language = Locale("ES")
+            "Estado funcional correcto"
+        } else "Algo salio mal,pruebe despues"
+
+        Toast.makeText(this, resultado, Toast.LENGTH_SHORT).show()
+        //ya funciona jajaja
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding=ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-
+        tts = TextToSpeech(this,this)
     }
 
     override fun onStart() {
-
         super.onStart()
-        // registrar el BroadcastRecivier
+        // Registrar el BroadcastReceiver
         registerReceiver(getAirplaneMode, IntentFilter(Intent.ACTION_AIRPLANE_MODE_CHANGED))
+        registerReceiver(getTimeChange, IntentFilter(Intent.ACTION_TIME_TICK))
+        registerReceiver(getWifiMode, IntentFilter(WifiManager.WIFI_STATE_CHANGED_ACTION))
     }
 
-
     override fun onStop() {
-
         super.onStop()
-        //para quitar el registro
+        // Quitar el registro
         unregisterReceiver(getAirplaneMode)
-        //:)
+        unregisterReceiver(getTimeChange)
     }
 
 }
+
+
